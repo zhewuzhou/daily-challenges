dbde8d216bb49d5fe471c8872b033496aa8d36f8
zhewuzhou <zhewuzhou@gmail.com>
2020-08-18 22:55:58 +0800

    [Z.Z][LeetCode] Add notes to the problem

--- src/main/kotlin/zhewuzhou/me/leetcode340/VerifyTree331.kt
+++ src/main/kotlin/zhewuzhou/me/leetcode340/VerifyTree331.kt
@@ -5,6 +5,12 @@ Consider the binary tree if count # node:
 1. number of edges = node+1
 2. At a given point, edges can not be negative
  */
+
+/*
+1. Play around the problem
+2. Time-box and discover those attribute via google or just play
+3. Time-box!!!
+ */
 fun isValidSerialization(preorder: String): Boolean {
     val nodes = preorder.split(',')
     var edges = 1
