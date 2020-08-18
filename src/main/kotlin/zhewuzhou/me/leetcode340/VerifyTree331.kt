316448f4c7f5fe107e037518f63f397b2874dbb5
zhewuzhou <zhewuzhou@gmail.com>
2020-08-18 22:45:02 +0800

    [Z.Z][LeetCode] Validate binary tree

---
+++ src/main/kotlin/zhewuzhou/me/leetcode340/VerifyTree331.kt
@@ -0,0 +1,19 @@
+package zhewuzhou.me.leetcode340
+
+/*
+Consider the binary tree if count # node:
+1. number of edges = node+1
+2. At a given point, edges can not be negative
+ */
+fun isValidSerialization(preorder: String): Boolean {
+    val nodes = preorder.toCharArray().filter { it != ',' }
+    var edges = 1
+    for (i in nodes.indices) {
+        edges-- // consume one edge
+        if (edges < 0) return false
+        if (nodes[i] != '#') {
+            edges += 2 // generate 2 edges
+        }
+    }
+    return edges == 0
+}
---
+++ src/test/kotlin/zhewuzhou/me/leetcode340/VerifyTree331KtTest.kt
@@ -0,0 +1,32 @@
+package zhewuzhou.me.leetcode340
+
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.*
+
+/*
+The pre order of the tree is: root -> left -> right
+1. Root can not be null
+ */
+
+internal class VerifyTree331KtTest {
+    private companion object {
+        @JvmStatic
+        fun cases() = Arrays.stream(
+            arrayOf(
+                Pair("9,3,4,#,#,1,#,#,2,#,6,#,#", true),
+                Pair("1,#", false),
+                Pair("9,#,#,1", false)
+
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("cases")
+    fun `Should verify if the given string can be tree pre order`(case: Pair<String, Boolean>) {
+        assertThat(isValidSerialization(case.first), `is`(case.second))
+    }
+}
