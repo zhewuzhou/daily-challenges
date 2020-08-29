19d791adec9ff74fd595bb59d055500c72057861
zhewuzhou <zhewuzhou@gmail.com>
2020-08-29 11:51:38 +0800

    [Z.Z][LeetCode] Vital important to remember path.removeAt(path.lastIndex)

--- src/main/kotlin/zhewuzhou/me/leetcode340/ReconstructItinerary332.kt
+++ src/main/kotlin/zhewuzhou/me/leetcode340/ReconstructItinerary332.kt
@@ -2,24 +2,31 @@ package zhewuzhou.me.leetcode340
 
 data class Ticket(var used: Boolean, val from: String, val to: String)
 
+
+/* It is vital to remember that for any dfs solution:
+    1. What is the different of
+    path.removeAt(path.lastIndex)
+    path.remove(n.to) or path.remove(path.last())
+    because there might be duplication, so always do path.removeAt(path.lastIndex)
+ */
 fun findItinerary(tickets: List<List<String>>): List<String> {
     if (tickets.isEmpty()) return listOf()
     val ts = tickets.map { Ticket(false, it[0], it[1]) }
 
-    fun next(path: MutableList<String>): List<Ticket> =
-        ts.filter { !it.used && it.from == path.last() }.sortedBy { it.to }
+    fun next(cur: String): List<Ticket> =
+        ts.filter { !it.used && it.from == cur }.sortedBy { it.to }
 
     fun doFind(path: MutableList<String>): List<String> {
         if (ts.all { it.used }) {
             return path
         }
-        val nextSteps = next(path)
+        val nextSteps = next(path.last())
         for (n in nextSteps) {
             path.add(n.to)
             n.used = true
             val result = doFind(path)
             if (result.isNotEmpty()) return result
-            path.remove(path.last())
+            path.removeAt(path.lastIndex)
             n.used = false
         }
         return listOf()
--- src/test/kotlin/zhewuzhou/me/leetcode340/ReconstructItinerary332KtTest.kt
+++ src/test/kotlin/zhewuzhou/me/leetcode340/ReconstructItinerary332KtTest.kt
@@ -11,22 +11,45 @@ internal class ReconstructItinerary332KtTest {
         @JvmStatic
         fun cases() = Arrays.stream(
             arrayOf(
+//                Pair(listOf(
+//                    listOf("MUC", "LHR"),
+//                    listOf("JFK", "MUC"),
+//                    listOf("SFO", "SJC"),
+//                    listOf("LHR", "SFO")
+//                ), listOf("JFK", "MUC", "LHR", "SFO", "SJC")),
+//                Pair(listOf(
+//                    listOf("JFK", "SFO"),
+//                    listOf("JFK", "ATL"),
+//                    listOf("SFO", "ATL"),
+//                    listOf("ATL", "JFK"),
+//                    listOf("ATL", "SFO")
+//                ), listOf("JFK", "ATL", "JFK", "SFO", "ATL", "SFO")),
+//                Pair(listOf(), listOf()),
+//                Pair(listOf(listOf("JFK", "ALT")), listOf("JFK", "ALT")),
+//                Pair(listOf(listOf("SFO", "ALT")), listOf()),
                 Pair(listOf(
-                    listOf("MUC", "LHR"),
-                    listOf("JFK", "MUC"),
-                    listOf("SFO", "SJC"),
-                    listOf("LHR", "SFO")
-                ), listOf("JFK", "MUC", "LHR", "SFO", "SJC")),
-                Pair(listOf(
-                    listOf("JFK", "SFO"),
-                    listOf("JFK", "ATL"),
-                    listOf("SFO", "ATL"),
-                    listOf("ATL", "JFK"),
-                    listOf("ATL", "SFO")
-                ), listOf("JFK", "ATL", "JFK", "SFO", "ATL", "SFO")),
-                Pair(listOf(), listOf()),
-                Pair(listOf(listOf("JFK", "ALT")), listOf("JFK", "ALT")),
-                Pair(listOf(listOf("SFO", "ALT")), listOf())
+                    listOf("EZE", "TIA"),
+                    listOf("EZE", "HBA"),
+                    listOf("AXA", "TIA"),
+                    listOf("JFK", "AXA"),
+                    listOf("ANU", "JFK"),
+                    listOf("ADL", "ANU"),
+                    listOf("TIA", "AUA"),
+                    listOf("ANU", "AUA"),
+                    listOf("ADL", "EZE"),
+                    listOf("ADL", "EZE"),
+                    listOf("EZE", "ADL"),
+                    listOf("AXA", "EZE"),
+                    listOf("AUA", "AXA"),
+                    listOf("JFK", "AXA"),
+                    listOf("AXA", "AUA"),
+                    listOf("AUA", "ADL"),
+                    listOf("ANU", "EZE"),
+                    listOf("TIA", "ADL"),
+                    listOf("EZE", "ANU"),
+                    listOf("AUA", "ANU")),
+                    listOf("JFK", "AXA", "AUA", "ADL", "ANU", "AUA", "ANU", "EZE", "ADL", "EZE",
+                        "ANU", "JFK", "AXA", "EZE", "TIA", "AUA", "AXA", "TIA", "ADL", "EZE", "HBA"))
             )
         )
     }
