d2543aebfc37e711350dfcaea9ef166ffae06411
zhewuzhou <zhewuzhou@gmail.com>
2020-08-30 18:20:27 +0800

    [Z.Z][LeetCode] Setup test for 336

---
+++ src/main/kotlin/zhewuzhou/me/leetcode340/PalindromePairs.kt
@@ -0,0 +1,5 @@
+package zhewuzhou.me.leetcode340
+
+fun palindromePairs(words: Array<String>): List<List<Int>> {
+    return listOf()
+}
---
+++ src/test/kotlin/zhewuzhou/me/leetcode340/PalindromePairsKtTest.kt
@@ -0,0 +1,36 @@
+package zhewuzhou.me.leetcode340
+
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.*
+
+internal class PalindromePairsKtTest {
+    private companion object {
+        @JvmStatic
+        fun cases() = Arrays.stream(
+            arrayOf(
+                Pair(arrayOf("abcd", "dcba", "lls", "s", "sssll"),
+                    listOf(listOf(0, 1), listOf(1, 0), listOf(3, 2), listOf(2, 4))
+                ),
+                Pair(arrayOf("bat", "tab", "cat"),
+                    listOf(listOf(0, 1), listOf(1, 0))
+                ),
+                Pair(arrayOf("a", ""),
+                    listOf(listOf(0, 1), listOf(1, 0))
+                )
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("cases")
+    fun `Should find all combinations of indexes that forms palindrome`(case: Pair<Array<String>, List<List<Int>>>) {
+        val result = palindromePairs(case.first)
+        assertThat(result.size, `is`(case.second.size))
+        for (p in result) {
+            assertThat(case.second.contains(p), `is`(true))
+        }
+    }
+}
