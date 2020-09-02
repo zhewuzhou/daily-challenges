1187fc8ce20dfa998fd12994f4aca73b075b2a5d
zhewuzhou <zhewuzhou@gmail.com>
2020-09-02 22:32:14 +0800

    [Z.Z][LeetCode] Stack solution of nested iterator

--- src/main/kotlin/zhewuzhou/me/datastructure/NestedIterator341.kt
+++ src/main/kotlin/zhewuzhou/me/datastructure/NestedIterator341.kt
@@ -1,8 +1,12 @@
 package zhewuzhou.me.datastructure
 
+import java.util.*
+
+
 class NestedInteger {
     constructor()
     constructor(value: Int)
+
     fun isInteger(): Boolean = false
     fun getInteger(): Int? = 0
     fun setInteger(value: Int): Unit {}
@@ -13,13 +17,14 @@ class NestedInteger {
 fun convertNestedIntegerToList(nested: NestedInteger): List<Int> = if (nested.isInteger()) {
     listOf(nested.getInteger()!!)
 } else {
+    1
     nested.getList()!!.flatMap { convertNestedIntegerToList(it) }
 }
 
 /*
 You can also using a stack to keep push iterator when there is nested integer until stack empty
  */
-class NestedIterator(nestedList: List<NestedInteger>) {
+class NestedIterator1(nestedList: List<NestedInteger>) {
     private var cur = 0
     private val flattedList: List<Int> = nestedList.flatMap { convertNestedIntegerToList(it) }
 
@@ -29,4 +34,31 @@ class NestedIterator(nestedList: List<NestedInteger>) {
     }
 
     fun hasNext(): Boolean = cur < flattedList.lastIndex
+}
+
+class NestedIterator(nestedList: List<NestedInteger>) : Iterator<Int> {
+    var stack = Stack<NestedInteger>()
+
+    init {
+        prepareStack(nestedList)
+    }
+
+    override fun next(): Int {
+        require(stack.isNotEmpty())
+        return stack.pop().getInteger()!!
+    }
+
+    override fun hasNext(): Boolean {
+        while (stack.isNotEmpty() && !stack.peek().isInteger()) {
+            val list = stack.pop().getList()!!
+            prepareStack(list)
+        }
+        return !stack.isEmpty()
+    }
+
+    private fun prepareStack(nestedList: List<NestedInteger>) {
+        for (i in nestedList.indices.reversed()) {
+            stack.push(nestedList[i])
+        }
+    }
 }
