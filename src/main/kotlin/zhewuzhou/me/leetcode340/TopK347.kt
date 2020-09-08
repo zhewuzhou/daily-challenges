819bf40f04c19393d0a4981acfccd7b420bfbed3
zhewuzhou <zhewuzhou@gmail.com>
2020-09-08 20:46:30 +0800

    [Z.Z][LeetCode] Test case for 347

---
+++ src/main/kotlin/zhewuzhou/me/leetcode340/TopK347.kt
@@ -0,0 +1,5 @@
+package zhewuzhou.me.leetcode340
+
+fun topKFrequent(nums: IntArray, k: Int): IntArray {
+    return intArrayOf()
+}
---
+++ src/test/kotlin/zhewuzhou/me/leetcode340/TopK347KtTest.kt
@@ -0,0 +1,25 @@
+package zhewuzhou.me.leetcode340
+
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.*
+
+internal class TopK347KtTest {
+    private companion object {
+        @JvmStatic
+        fun cases() = Arrays.stream(
+            arrayOf(
+                Triple(intArrayOf(1, 1, 1, 2, 2, 3), 2, listOf(1, 2)),
+                Triple(intArrayOf(1), 1, listOf(1))
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("cases")
+    fun `Should calculate top k elements`(case: Triple<IntArray, Int, List<Int>>) {
+        assertThat(topKFrequent(case.first, case.second).toList().sorted(), `is`(case.third))
+    }
+}
