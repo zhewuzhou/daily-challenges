15c8aa654ff8b3875f0c390c732bbaafc38ecb91
zhewuzhou <zhewuzhou@gmail.com>
2020-09-10 23:07:26 +0800

    [Z.Z][LeetCode] The pre process actually makes the move joint

--- src/main/kotlin/zhewuzhou/me/dp/WordLadder.kt
+++ src/main/kotlin/zhewuzhou/me/dp/WordLadder.kt
@@ -1,38 +1,35 @@
 package zhewuzhou.me.dp
 
 import java.util.*
-import java.util.function.Consumer
 
 fun ladderLength(beginWord: String, endWord: String, wordList: List<String>): Int {
     val wordLen = beginWord.length
-    val allComboDict: MutableMap<String, MutableList<String>> = HashMap()
-    wordList.forEach(
-        Consumer { word: String ->
-            for (i in 0 until wordLen) {
-                val newWord = word.substring(0, i) + '*' + word.substring(i + 1, wordLen)
-                val transformations = allComboDict.getOrDefault(newWord, ArrayList())
-                transformations.add(word)
-                allComboDict[newWord] = transformations
-            }
-        })
-
+    val allComboDict = mutableMapOf<String, MutableList<String>>()
+    wordList.forEach {
+        for (i in 0 until wordLen) {
+            val newWord = it.substring(0, i) + '*' + it.substring(i + 1, wordLen)
+            val transforms = allComboDict.getOrDefault(newWord, mutableListOf())
+            transforms.add(it)
+            allComboDict[newWord] = transforms
+        }
+    }
     val queue = LinkedList<Pair<String, Int>>()
     queue.add(Pair(beginWord, 1))
     val visited = mutableMapOf<String, Boolean>()
     visited[beginWord] = true
     while (!queue.isEmpty()) {
         val node = queue.remove()
-        val word: String = node.first
-        val level: Int = node.second
+        val word = node.first
+        val level = node.second
         for (i in 0 until wordLen) {
             val newWord = word.substring(0, i) + '*' + word.substring(i + 1, wordLen)
-            for (adjacentWord in allComboDict.getOrDefault(newWord, ArrayList())) {
-                if (adjacentWord == endWord) {
+            for (next in allComboDict.getOrDefault(newWord, ArrayList())) {
+                if (next == endWord) {
                     return level + 1
                 }
-                if (!visited.containsKey(adjacentWord)) {
-                    visited[adjacentWord] = true
-                    queue.add(Pair(adjacentWord, level + 1))
+                if (!visited.containsKey(next)) {
+                    visited[next] = true
+                    queue.add(Pair(next, level + 1))
                 }
             }
         }
