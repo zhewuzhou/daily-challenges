22dda411a59b047085f83ec50aa8752c32d89ad2
zhewuzhou <zhewuzhou@gmail.com>
2020-09-13 18:31:04 +0800

    [Z.Z][LeetCode] Code for single atom

--- src/main/kotlin/zhewuzhou/me/numbers/NumsOfAtoms726.kt
+++ src/main/kotlin/zhewuzhou/me/numbers/NumsOfAtoms726.kt
@@ -3,3 +3,7 @@ package zhewuzhou.me.numbers
 fun countOfAtoms(formula: String): String {
     return ""
 }
+
+fun handleSingleAtom(atomFormula: String): Pair<String, Int> {
+    return Pair("", 0)
+}
--- src/test/kotlin/zhewuzhou/me/numbers/NumsOfAtoms726KtTest.kt
+++ src/test/kotlin/zhewuzhou/me/numbers/NumsOfAtoms726KtTest.kt
@@ -17,18 +17,35 @@ Requirements:
 internal class NumsOfAtoms726KtTest {
     private companion object {
         @JvmStatic
-        fun cases() = Arrays.stream(
+        fun formulaCases() = Arrays.stream(
             arrayOf(
                 Pair("H2O", "H2O"),
                 Pair("Mg(OH)2", "H2MgO2"),
                 Pair("K4(ON(SO3)2)2", "K4N2O14S4")
             )
         )
+
+        @JvmStatic
+        fun atomCases() = Arrays.stream(
+            arrayOf(
+                Pair("H", Pair("H", 1)),
+                Pair("O2", Pair("O", 2)),
+                Pair("Mg2", Pair("Mg", 2))
+            )
+        )
     }
 
     @ParameterizedTest
-    @MethodSource("cases")
+    @MethodSource("formulaCases")
     fun `Should find all the numbers of atoms`(case: Pair<String, String>) {
         assertThat(countOfAtoms(case.first), `is`(case.second))
+    }
+
+    @ParameterizedTest
+    @MethodSource("atomCases")
+    fun `Should count single atoms`(case: Pair<String, Pair<String, Int>>) {
+        val result = handleSingleAtom(case.first)
+        assertThat(result.first, `is`(case.second.first))
+        assertThat(result.second, `is`(case.second.second))
     }
 }
