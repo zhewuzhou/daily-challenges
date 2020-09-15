9bd7ecbeb559c15d078e3208cd0f0c2fec425019
zhewuzhou <zhewuzhou@gmail.com>
2020-09-15 21:29:17 +0800

    [Z.Z][LeetCode] Test for perfect rectangle problem

--- src/test/kotlin/zhewuzhou/me/numbers/PerfectRectangle391KtTest.kt
+++ src/test/kotlin/zhewuzhou/me/numbers/PerfectRectangle391KtTest.kt
@@ -6,19 +6,33 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.*
 
-internal class PerfectRectangle391KtTest{
+/*
+1. N rectangles such that N>0
+2. If N rectangles form exact one rectangle
+    - Can not overlap
+    - Exact!!!
+ */
+
+internal class PerfectRectangle391KtTest {
     private companion object {
         @JvmStatic
         fun cases() = Arrays.stream(
             arrayOf(
-                Pair(1, 1)
+                Pair(arrayOf(intArrayOf(1, 1, 3, 3), intArrayOf(3, 1, 4, 2),
+                    intArrayOf(3, 2, 4, 4), intArrayOf(1, 3, 2, 4), intArrayOf(2, 3, 3, 4)), true),
+                Pair(arrayOf(intArrayOf(1, 1, 2, 3), intArrayOf(1, 3, 2, 4),
+                    intArrayOf(3, 1, 4, 2), intArrayOf(3, 2, 4, 4)), false),
+                Pair(arrayOf(intArrayOf(1, 1, 3, 3), intArrayOf(3, 1, 4, 2),
+                    intArrayOf(1, 3, 2, 4), intArrayOf(3, 2, 4, 4)), false),
+                Pair(arrayOf(intArrayOf(1, 1, 3, 3), intArrayOf(3, 1, 4, 2),
+                    intArrayOf(1, 3, 2, 4), intArrayOf(2, 2, 4, 4)), false)
             )
         )
     }
 
     @ParameterizedTest
     @MethodSource("cases")
-    fun `Should determine if there is perfect rectangle`(case: Pair<Int, Int>) {
-        assertThat(case.first, `is`(case.second))
+    fun `Should determine if there is perfect rectangle`(case: Pair<Array<IntArray>, Boolean>) {
+        assertThat(isRectangleCover(case.first), `is`(case.second))
     }
 }
