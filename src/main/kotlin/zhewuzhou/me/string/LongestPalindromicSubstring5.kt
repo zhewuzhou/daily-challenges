71fff0ca87b6535fd7700256543f09ba8d556966
zhewuzhou <zhewuzhou@gmail.com>
2020-09-21 22:47:36 +0800

    [Z.Z][LeetCode] merge 5th challenge

--- src/main/kotlin/zhewuzhou/me/string/LongestPalindromicSubstring5.kt
+++ src/main/kotlin/zhewuzhou/me/string/LongestPalindromicSubstring5.kt
@@ -5,5 +5,25 @@ DP Solution
  */
 
 fun longestPalindrome(s: String): String {
-    return ""
+    var res = ""
+    val metrics = Array(s.length) {
+        BooleanArray(s.length) {
+            false
+        }
+    }
+    /*
+     * The key is search a small part first
+     * Incremental the search space smoothly
+     */
+    for (i in s.lastIndex downTo 0) {
+        for (j in (i + 1)..s.lastIndex) {
+            metrics[i][j] = (s[i] == s[j]) && (j - i < 3 || metrics[i + 1][j - 1])
+            if (metrics[i][j]) {
+                if (j - i + 1 > res.length) {
+                    res = s.substring(i, j + 1)
+                }
+            }
+        }
+    }
+    return res
 }
