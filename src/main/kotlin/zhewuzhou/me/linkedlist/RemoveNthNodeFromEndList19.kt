f0017c89712c8b1b687c126094488054c91b39ab
zhewuzhou <zhewuzhou@gmail.com>
2020-09-23 17:29:07 +0800

    [Z.Z][Refactor] Add patterns for this type of challenges

--- src/main/kotlin/zhewuzhou/me/linkedlist/RemoveNthNodeFromEndList19.kt
+++ src/main/kotlin/zhewuzhou/me/linkedlist/RemoveNthNodeFromEndList19.kt
@@ -1,14 +1,19 @@
 package zhewuzhou.me.linkedlist
 
 fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+    /*Pattern1: dummy act as the anchor given list node could be challenged*/
     val dummy: ListNode? = ListNode(1)
     dummy?.next = head
+
+    /*Pattern2: fast/slow pointer to locate specific pos*/
     var fast = head
+    var slow = head
+    var preSlow = dummy
+
+    /*Pattern3: should run it in brain for small list*/
     for (i in 1 until n) {
         fast = fast?.next
     }
-    var slow = head
-    var preSlow = dummy
     while (fast?.next != null) {
         fast = fast.next
         preSlow = slow
