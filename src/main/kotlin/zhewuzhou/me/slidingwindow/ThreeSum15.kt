57a9cae2c7fb74a98db1f8cd1ddee32e992a2758
zhewuzhou <zhewuzhou@gmail.com>
2020-09-23 19:30:32 +0800

    [Z.Z][Refactor] 15 & 18 code reuse setup

--- src/main/kotlin/zhewuzhou/me/slidingwindow/ThreeSum15.kt
+++ src/main/kotlin/zhewuzhou/me/slidingwindow/ThreeSum15.kt
@@ -2,22 +2,22 @@ package zhewuzhou.me.slidingwindow
 
 fun threeSum(nums: IntArray): List<List<Int>> {
     nums.sort()
-    return threeSum(nums, 0)
+    return threeSum(nums, 0, 0)
 }
 
-fun threeSum(nums: IntArray, target: Int): MutableList<MutableList<Int>> {
+fun threeSum(nums: IntArray, target: Int, start: Int): MutableList<MutableList<Int>> {
     val result = mutableListOf<MutableList<Int>>()
-    var ni = 0
-    while (ni < nums.size - 2) {
-        val remaining = target - nums[ni]
-        var left = ni + 1
+    var lower = start
+    while (lower < nums.size - 2) {
+        val t = target - nums[lower]
+        var left = lower + 1
         var right = nums.size - 1
         while (left < right) {
-            when ((nums[left] + nums[right]).compareTo(remaining)) {
+            when ((nums[left] + nums[right]).compareTo(t)) {
                 1 -> right -= 1
                 -1 -> left += 1
                 0 -> {
-                    result.add(mutableListOf(nums[ni], nums[left], nums[right]))
+                    result.add(mutableListOf(nums[lower], nums[left], nums[right]))
                     while (left < right && nums[left] == nums[left + 1]) {
                         left += 1
                     }
@@ -29,10 +29,10 @@ fun threeSum(nums: IntArray, target: Int): MutableList<MutableList<Int>> {
                 }
             }
         }
-        while (ni < nums.size - 1 && nums[ni] == nums[ni + 1]) {
-            ni += 1
+        while (lower < nums.size - 1 && nums[lower] == nums[lower + 1]) {
+            lower += 1
         }
-        ni += 1
+        lower += 1
     }
     return result
 }
