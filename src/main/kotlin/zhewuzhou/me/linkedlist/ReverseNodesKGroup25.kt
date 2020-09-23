30121afed5741b2b241b9557c5a6e16c0d45cbc2
zhewuzhou <zhewuzhou@gmail.com>
2020-09-23 20:39:59 +0800

    [Z.Z][LeetCode] Normalization of 25th challenge

---
+++ src/main/kotlin/zhewuzhou/me/linkedlist/ReverseNodesKGroup25.kt
@@ -0,0 +1,22 @@
+package zhewuzhou.me.linkedlist
+
+fun reverseKGroup(head: ListNode?, k: Int): ListNode? {
+    var curSecHead = head
+    var nextSecHead = curSecHead
+    var count = 0
+    while (nextSecHead != null && count != k) {
+        nextSecHead = nextSecHead.next
+        count++
+    }
+    if (count == k) {
+        nextSecHead = reverseKGroup(nextSecHead, k)
+        while (count-- > 0) {
+            val tmp = curSecHead!!.next
+            curSecHead.next = nextSecHead
+            nextSecHead = curSecHead
+            curSecHead = tmp
+        }
+        curSecHead = nextSecHead
+    }
+    return curSecHead
+}
---
+++ src/test/kotlin/zhewuzhou/me/linkedlist/ReverseNodesKGroup25KtTest.kt
@@ -0,0 +1,37 @@
+package zhewuzhou.me.linkedlist
+
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import zhewuzhou.me.linkedlist.ListNode
+import zhewuzhou.me.linkedlist.reverseKGroup
+import zhewuzhou.me.linkedlist.toListNode
+import java.util.*
+
+internal class ReverseNodesKGroup25KtTest {
+    private companion object {
+        @JvmStatic
+        fun cases() = Arrays.stream(
+            arrayOf(
+                Triple(listOf(1, 2, 3, 4, 5).toListNode(), 2, listOf(2, 1, 4, 3, 5).toListNode()),
+                Triple(listOf(1, 2, 3, 4, 5).toListNode(), 3, listOf(3, 2, 1, 4, 5).toListNode()),
+                Triple(listOf(1, 2).toListNode(), 2, listOf(2, 1).toListNode())
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("cases")
+    fun `Should reverse k elements`(case: Triple<ListNode?, Int, ListNode?>) {
+        var result = reverseKGroup(case.first, case.second)
+        var expect = case.third
+        while (result != null && expect != null) {
+            assertThat(result.`val`, `is`(expect.`val`))
+            result = result.next
+            expect = expect.next
+        }
+        assertThat(result == null, `is`(true))
+        assertThat(expect == null, `is`(true))
+    }
+}
