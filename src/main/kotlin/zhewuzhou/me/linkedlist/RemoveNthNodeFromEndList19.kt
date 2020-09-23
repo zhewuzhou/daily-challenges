51b8a560af52e3b4c34820dc7fca6b47ab6f056d
zhewuzhou <zhewuzhou@gmail.com>
2020-09-23 17:25:06 +0800

    [Z.Z][ReWrite] Re implment the linked list challenge of remove nth element from the last

--- src/main/kotlin/zhewuzhou/me/linkedlist/RemoveNthNodeFromEndList19.kt
+++ src/main/kotlin/zhewuzhou/me/linkedlist/RemoveNthNodeFromEndList19.kt
@@ -1,33 +1,20 @@
 package zhewuzhou.me.linkedlist
 
 fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
-    val length = listNodeLength(head)
-    return when (length == n) {
-        true -> {
-            val nh = head?.next
-            head?.next = null
-            nh
-        }
-        false -> {
-            var before = head
-            var pc = 1
-            while (pc < length - n) {
-                pc += 1
-                before = before?.next
-            }
-            val toRemove = before?.next
-            before?.next = toRemove?.next
-            head
-        }
+    val dummy: ListNode? = ListNode(1)
+    dummy?.next = head
+    var fast = head
+    for (i in 1 until n) {
+        fast = fast?.next
     }
+    var slow = head
+    var preSlow = dummy
+    while (fast?.next != null) {
+        fast = fast.next
+        preSlow = slow
+        slow = slow?.next
+    }
+    preSlow?.next = slow?.next
+    return dummy?.next
 }
 
-private fun listNodeLength(head: ListNode?): Int {
-    var nh = head
-    var length = 1
-    while (nh?.next != null) {
-        nh = nh.next
-        length += 1
-    }
-    return length
-}
--- src/test/kotlin/zhewuzhou/me/linkedlist/RemoveNthNodeFromEndList19KtTest.kt
+++ src/test/kotlin/zhewuzhou/me/linkedlist/RemoveNthNodeFromEndList19KtTest.kt
@@ -1,56 +1,35 @@
 package zhewuzhou.me.linkedlist
 
-import org.hamcrest.CoreMatchers
-import org.hamcrest.MatcherAssert
-import org.junit.jupiter.api.Test
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.*
 
 internal class RemoveNthNodeFromEndList19KtTest {
+    private companion object {
+        @JvmStatic
+        fun cases() = Arrays.stream(
+            arrayOf(
+                Triple(listOf(1, 2, 3, 4, 5).toListNode(), 2, listOf(1, 2, 3, 5).toListNode()),
+                Triple(listOf(1, 2, 3, 4, 5).toListNode(), 5, listOf(2, 3, 4, 5).toListNode()),
+                Triple(listOf<Int>().toListNode(), 0, listOf<Int>().toListNode()),
+                Triple(listOf(1).toListNode(), 1, listOf<Int>().toListNode())
+            )
+        )
+    }
 
-    @Test
-    fun `Should remove the n element from tail`() {
-        val head = createTestList()
-        var newHead = removeNthFromEnd(head, 2)
-        var li = 1
-        while (li < 5) {
-            when (li == 4) {
-                true -> MatcherAssert.assertThat(newHead?.`val`, CoreMatchers.`is`(li + 1))
-                false -> MatcherAssert.assertThat(newHead?.`val`, CoreMatchers.`is`(li))
-            }
-            li++
-            newHead = newHead?.next
+    @ParameterizedTest
+    @MethodSource("cases")
+    fun `Should remove nth node from the end of the list`(case: Triple<ListNode?, Int, ListNode?>) {
+        var result = removeNthFromEnd(case.first, case.second)
+        var expect = case.third
+        while (result != null && expect != null) {
+            assertThat(result.`val`, `is`(expect.`val`))
+            result = result.next
+            expect = expect.next
         }
-    }
-
-    @Test
-    fun `Should remove the n element from tail edge case remove head`() {
-        val head = createTestList()
-        val newHead = removeNthFromEnd(head, 5)
-        MatcherAssert.assertThat(newHead?.`val`, CoreMatchers.`is`(2))
-    }
-
-    @Test
-    fun `Should remove the n element from tail edge case null`() {
-        val newHead = removeNthFromEnd(null, 0)
-        MatcherAssert.assertThat(newHead?.`val` == null, CoreMatchers.`is`(true))
-    }
-
-    @Test
-    fun `Should remove the n element from tail edge case single element`() {
-        val head = ListNode(1)
-        val newHead = removeNthFromEnd(head, 1)
-        MatcherAssert.assertThat(newHead?.`val` == null, CoreMatchers.`is`(true))
-    }
-
-    private fun createTestList(): ListNode {
-        var li5 = ListNode(5)
-        var li4 = ListNode(4)
-        var li3 = ListNode(3)
-        var li2 = ListNode(2)
-        var li1 = ListNode(1)
-        li1.next = li2
-        li2.next = li3
-        li3.next = li4
-        li4.next = li5
-        return li1
+        assertThat(result == null, `is`(true))
+        assertThat(expect == null, `is`(true))
     }
 }
