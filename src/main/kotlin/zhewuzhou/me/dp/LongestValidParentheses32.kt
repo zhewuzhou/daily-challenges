8036f4e3f47e2ecfa317038758e932e9ab5412f8
zhewuzhou <zhewuzhou@gmail.com>
2020-09-25 13:05:41 +0800

    [Z.Z][Refactor] normalization of the 32th challenge

--- src/main/kotlin/zhewuzhou/me/dp/LongestValidParentheses32.kt
+++ src/main/kotlin/zhewuzhou/me/dp/LongestValidParentheses32.kt
@@ -1,4 +1,4 @@
-package zhewuzhou.me.leetcode40
+package zhewuzhou.me.dp
 
 /*
     TODO: 1. Stack spam
@@ -10,14 +10,14 @@ package zhewuzhou.me.leetcode40
 fun longestValidParentheses(s: String): Int {
     if (s.length <= 1) return 0;
     var curMax = 0;
-    val longest = IntArray(s.length)
+    val metrics = IntArray(s.length)
     for (i in 1 until s.length) {
         if (s[i] == ')' &&
-            i - longest[i - 1] - 1 >= 0 &&
-            s[i - longest[i - 1] - 1] == '(') {
-            longest[i] = longest[i - 1] + 2
-            longest[i] += if (i - longest[i - 1] - 2 >= 0) longest[i - longest[i - 1] - 2] else 0
-            curMax = Math.max(longest[i], curMax);
+            i - metrics[i - 1] - 1 >= 0 &&
+            s[i - metrics[i - 1] - 1] == '(') {
+            metrics[i] = metrics[i - 1] + 2
+            metrics[i] += if (i - metrics[i - 1] - 2 >= 0) metrics[i - metrics[i - 1] - 2] else 0
+            curMax = Math.max(metrics[i], curMax);
         }
     }
     return curMax;
--- src/test/kotlin/zhewuzhou/me/dp/LongestValidParentheses32KtTest.kt
+++ src/test/kotlin/zhewuzhou/me/dp/LongestValidParentheses32KtTest.kt
@@ -1,11 +1,11 @@
-package zhewuzhou.me.leetcode40
+package zhewuzhou.me.dp
 
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
+import zhewuzhou.me.dp.longestValidParentheses
 
-class LeetCode32KtTest {
-
+internal class LongestValidParentheses32KtTest {
     @Test
     fun `Should return 2 from (()`() {
         assertThat(longestValidParentheses("(()"), `is`(2))
