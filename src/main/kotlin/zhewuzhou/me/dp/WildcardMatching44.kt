9ba94315bf1ea9a20745b790da6dea31eac6eb1f
zhewuzhou <zhewuzhou@gmail.com>
2020-09-26 19:57:08 +0800

    [Z.Z][Refactor] Normalization of the 44th challenge

---
+++ src/main/kotlin/zhewuzhou/me/dp/WildcardMatching44.kt
@@ -0,0 +1,63 @@
+package zhewuzhou.me.dp
+
+/*
+1. Come up with recursive solution first
+2. Change it to cacheable metrics[][]
+ */
+
+fun isMatch44(s: String, p: String): Boolean {
+    val caches = mutableMapOf<Pair<Int, Int>, Boolean>()
+    fun match(s: String, p: String, si: Int, pi: Int): Boolean {
+        if (pi == p.length) return si == s.length
+        val key = Pair(si, pi)
+        if (!caches.containsKey(key)) {
+            caches[key] = when (p[pi]) {
+                '*' -> {
+                    (si < s.length && match(s, p, si + 1, pi))
+                        || match(s, p, si, pi + 1)
+                }
+                else -> {
+                    si < s.length
+                        && charMatch44(s[si], p[pi])
+                        && match(s, p, si + 1, pi + 1)
+                }
+            }
+        }
+        return caches[key]!!
+    }
+    return match(s, p, 0, 0)
+}
+
+
+fun isMatch44DP(s: String, p: String): Boolean {
+    val dp = Array(s.length + 1) { BooleanArray(p.length + 1) }
+    dp[0][0] = true
+    for (pi in p.indices) {
+        if (p[pi] == '*') {
+            dp[0][pi + 1] = true
+        } else {
+            break
+        }
+    }
+    for (pi in p.indices) {
+        for (si in s.indices) {
+            when (p[pi] == '*') {
+                true -> {
+                    dp[si + 1][pi + 1] = dp[si + 1][pi] || dp[si][pi + 1]
+                }
+                false -> {
+                    if (charMatch44(s[si], p[pi])) {
+                        dp[si + 1][pi + 1] = dp[si][pi]
+                    }
+                }
+            }
+        }
+    }
+    return dp[s.length][p.length]
+}
+
+private fun charMatch44(s: Char, p: Char): Boolean =
+    when (p == '?') {
+        true -> true
+        false -> s == p
+    }
---
+++ src/test/kotlin/zhewuzhou/me/dp/WildcardMatching44KtTest.kt
@@ -0,0 +1,33 @@
+package zhewuzhou.me.dp
+
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.*
+
+internal class WildcardMatching44KtTest {
+    private companion object {
+        val VERY_LONG_S = "aaaabaaaabbbbaabbbaabbaababbabbaaaababaaabbbbbbaabbbabababbaaabaabaaaaaabbaabbbbaababbababaabbbaababbbba"
+        val STRANGE_P = "*****b*aba***babaa*bbaba***a*aaba*b*aa**a*b**ba***a*a*"
+
+        @JvmStatic
+        fun cases() = Arrays.stream(
+            arrayOf(
+                Triple("aa", "a", false),
+                Triple("aa", "*", true),
+                Triple("cb", "?a", false),
+                Triple("adceb", "*a*b", true),
+                Triple(VERY_LONG_S, STRANGE_P, true),
+                Triple("acdcb", "a*c?b", false),
+                Triple("abefcdgiescdfimde", "ab*cd?i*de", true)
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("cases")
+    fun `Should match cases`(case: Triple<String, String, Boolean>) {
+        assertThat(isMatch44(case.first, case.second), `is`(case.third))
+    }
+}
