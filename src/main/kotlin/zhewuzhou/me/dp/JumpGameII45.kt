7318370282467ae5db31fb96e5c38ed3db3ea9ce
zhewuzhou <zhewuzhou@gmail.com>
2020-09-27 21:59:26 +0800

    [Z.Z][Refactor] Norliazation of the 45th challenge

---
+++ src/main/kotlin/zhewuzhou/me/dp/JumpGameII45.kt
@@ -0,0 +1,37 @@
+package zhewuzhou.me.dp
+
+fun jumpDP(nums: IntArray): Int {
+    if (nums.isEmpty()) return 0
+    val steps = IntArray(nums.size)
+    for (i in (nums.size - 2) downTo 0) {
+        when (nums[i] >= nums.lastIndex - i) {
+            true -> steps[i] = 1
+            false -> {
+                var minSteps = nums.size + 1
+                for (j in (i + 1)..(i + nums[i])) {
+                    minSteps = Math.min((1 + steps[j]), minSteps)
+                }
+                steps[i] = minSteps
+            }
+        }
+    }
+    return steps[0]
+}
+
+fun jump(nums: IntArray): Int {
+    var steps = 0
+    var end = 0
+    var start = 0
+    var maxEnd = 0
+    while (end < nums.lastIndex) {
+        steps += 1
+        maxEnd = end + 1
+        for (i in start..end) {
+            if (i + nums[i] > nums.lastIndex) return steps
+            maxEnd = Math.max(maxEnd, i + nums[i])
+        }
+        start = end + 1
+        end = maxEnd
+    }
+    return steps
+}
--- src/test/kotlin/zhewuzhou/me/dp/JumpGameII45KtTest.kt
+++ src/test/kotlin/zhewuzhou/me/dp/JumpGameII45KtTest.kt
@@ -1,20 +1,20 @@
-package zhewuzhou.me.leetcode60
+package zhewuzhou.me.dp
 
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
+import zhewuzhou.me.dp.jump
 import java.util.*
 
-class LeetCode45KtTest {
+class JumpGameII45KtTest {
     private companion object {
         @JvmStatic
         fun cases() = Arrays.stream(
             arrayOf(
                 Pair(intArrayOf(2, 3, 1, 1, 4), 2),
-                Pair(intArrayOf(2, 3, 0, 1, 4), 2),
                 Pair(intArrayOf(1, 2, 1, 1, 1), 3),
-                Pair(intArrayOf(3, 2, 1, 0, 4), 6)
+                Pair(intArrayOf(2, 3, 0, 1, 4), 2)
             )
         )
     }
