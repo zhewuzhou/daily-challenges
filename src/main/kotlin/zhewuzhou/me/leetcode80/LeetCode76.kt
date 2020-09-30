27b9714e11d7f1b60223ce4286e45b5f7799a0bf
zhewuzhou <zhewuzhou@gmail.com>
2020-09-30 13:02:07 +0800

    [Z.Z][Rewrite] Implement much faster solution basedon sliding window

--- src/main/kotlin/zhewuzhou/me/leetcode80/LeetCode76.kt
+++ src/main/kotlin/zhewuzhou/me/leetcode80/LeetCode76.kt
@@ -2,7 +2,41 @@ package zhewuzhou.me.leetcode80
 
 import java.util.*
 
-fun minWindow(src: String, t: String): String {
+
+fun minWindow(s: String, t: String): String {
+    val countMap = IntArray(128)
+    for (c in t.toCharArray()) {
+        countMap[c.toInt()]++
+    }
+    var left = 0
+    var right = 0
+    var minLeft = 0
+    var minLen = Int.MAX_VALUE
+    var counter = t.length
+    while (right < s.length) {
+        val c1 = s[right]
+        if (countMap[c1.toInt()] > 0) counter--
+        countMap[c1.toInt()]--
+        right++
+        while (counter == 0) {
+            if (minLen > right - left) {
+                minLen = right - left
+                minLeft = left
+            }
+            val c2 = s[left]
+            countMap[c2.toInt()]++
+            if (countMap[c2.toInt()] > 0) counter++
+            left++
+        }
+    }
+    return if (minLen == Int.MAX_VALUE) "" else s.substring(minLeft, minLeft + minLen)
+}
+
+/**
+ * This is very slow version of implementation
+ */
+
+fun minWindowDFS(src: String, t: String): String {
     if (t.length > src.length) return ""
     val target = t.associate { key -> Pair(key, t.count { it == key }) }.toMap()
     val count = mutableMapOf<Char, Int>()
--- src/test/kotlin/zhewuzhou/me/leetcode80/LeetCode76KtTest.kt
+++ src/test/kotlin/zhewuzhou/me/leetcode80/LeetCode76KtTest.kt
@@ -7,27 +7,21 @@ import org.junit.jupiter.params.provider.MethodSource
 import java.util.*
 
 internal class LeetCode76KtTest {
-    data class Test76Case(
-        val src: String,
-        val target: String,
-        val expected: String
-    )
-
     private companion object {
         @JvmStatic
         fun cases() = Arrays.stream(
             arrayOf(
-                Test76Case("ADOBECODEBANC", "ABC", "BANC"),
-                Test76Case("aa", "aa", "aa"),
-                Test76Case("a", "a", "a"),
-                Test76Case("ABXYZACBD", "ABC", "ACB")
+                Triple("ADOBECODEBANC", "ABC", "BANC"),
+                Triple("aa", "aa", "aa"),
+                Triple("a", "a", "a"),
+                Triple("ABXYZACBD", "ABC", "ACB")
             )
         )
     }
 
     @ParameterizedTest
     @MethodSource("cases")
-    fun `Should calculate min window`(case: Test76Case) {
-        assertThat(minWindow(case.src, case.target), `is`(case.expected))
+    fun `Should calculate min window`(case: Triple<String, String, String>) {
+        assertThat(minWindow(case.first, case.second), `is`(case.third))
     }
 }
