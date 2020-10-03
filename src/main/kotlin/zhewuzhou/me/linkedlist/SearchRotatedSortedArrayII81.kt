89552d62e60d1d292e467f7ab99af58e9a99472d
zhewuzhou <zhewuzhou@gmail.com>
2020-10-03 22:07:13 +0800

    [Z.Z][Refactor] Normalization of the 81st challenge

---
+++ src/main/kotlin/zhewuzhou/me/linkedlist/SearchRotatedSortedArrayII81.kt
@@ -0,0 +1,78 @@
+package zhewuzhou.me.linkedlist
+
+/*
+Actually nums.any {it == target} is't slow at all
+ */
+class RotatedArraySearcher {
+    fun search(nums: IntArray, target: Int): Boolean {
+        var s = 0
+        var e = nums.size
+        while (s < e) {
+            val mid = (s + e) / 2
+            if (nums[mid] == target) return true
+            if (nums[mid] > nums[s]) {
+                if (target < nums[mid] && target >= nums[s]) e = mid else s = mid + 1
+            } else if (nums[mid] < nums[s]) {
+                if (target > nums[mid] && target < nums[s]) s = mid + 1 else e = mid
+            } else {
+                s++
+            }
+        }
+        return false
+    }
+
+    fun searchB(nums: IntArray, target: Int): Boolean {
+        if (nums.isEmpty()) return false
+        nums.any { it == target }
+        var start = 0
+        var end = nums.lastIndex
+        if (target < nums[0] && target > nums[end]) {
+            return false
+        } else if (target >= nums[0] && target > nums[end]) {
+            while (end > 0 && nums[end] >= nums[end - 1]) end -= 1
+            return binarySearch(nums, 0, end - 1, target)
+        } else if (target < nums[0] && target <= nums[end]) {
+            while (start < nums.lastIndex && nums[start] <= nums[start + 1]) start += 1
+            return binarySearch(nums, start + 1, end, target)
+        } else {
+            while (start <= end) {
+                if (nums[start] == target || nums[end] == target) return true
+                if (nums[start] <= nums[start + 1]) {
+                    start += 1
+                } else {
+                    return binarySearch(nums, start + 1, end, target)
+                }
+                if (nums[end] >= nums[end - 1]) {
+                    end -= 1
+                } else {
+                    return binarySearch(nums, start, end - 1, target)
+                }
+            }
+            return false
+        }
+    }
+
+    fun binarySearch(nums: IntArray, s: Int, e: Int, target: Int): Boolean {
+        var start = s
+        var end = e
+        while (start <= end) {
+            var mid = (start + end + 1) / 2
+            when (nums[mid].compareTo(target)) {
+                0 -> return true
+                1 -> {
+                    while (mid > s && nums[mid] == nums[mid - 1]) mid -= 1
+                    end = mid - 1
+                }
+                -1 -> {
+                    while (mid < e && nums[mid] == nums[mid + 1]) mid += 1
+                    start = mid + 1
+                }
+            }
+        }
+        return false
+    }
+}
+
+
+
+
--- src/test/kotlin/zhewuzhou/me/numberssort/SearchRotatedSortedArrayII81KtTest.kt
+++ src/test/kotlin/zhewuzhou/me/numberssort/SearchRotatedSortedArrayII81KtTest.kt
@@ -1,12 +1,13 @@
-package zhewuzhou.me.leetcode100
+package zhewuzhou.me.numberssort
 
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
+import zhewuzhou.me.linkedlist.RotatedArraySearcher
 import java.util.*
 
-class LeetCode81KtTest {
+internal class SearchRotatedSortedArrayII81KtTest {
     private companion object {
         @JvmStatic
         fun cases() = Arrays.stream(
@@ -28,6 +29,7 @@ class LeetCode81KtTest {
     @MethodSource("cases")
     fun `Should find target in rotated array`(case: Triple<List<Int>, Int, Boolean>) {
         val nums = case.first.toIntArray()
-        assertThat(search(nums, case.second), `is`(case.third))
+        val finder = RotatedArraySearcher()
+        assertThat(finder.search(nums, case.second), `is`(case.third))
     }
 }
