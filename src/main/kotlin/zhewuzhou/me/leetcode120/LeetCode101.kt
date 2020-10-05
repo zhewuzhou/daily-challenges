669859071e29459e2b5437e6cd3498261c8321e0
zhewuzhou <zhewuzhou@gmail.com>
2020-10-05 15:15:05 +0800

    [Z.Z][Refactor] Move stack solution to it's place

--- src/main/kotlin/zhewuzhou/me/leetcode120/LeetCode101.kt
+++ src/main/kotlin/zhewuzhou/me/leetcode120/LeetCode101.kt
@@ -3,47 +3,6 @@ package zhewuzhou.me.leetcode120
 import zhewuzhou.utils.TreeNode
 import java.util.*
 
-fun isSymmetric(root: TreeNode?): Boolean {
-    val lhs = root?.left
-    val rhs = root?.right
-    if (lhs == null && rhs == null) return true
-    if (lhs == null || rhs == null) return false
-    return isSymmetric(root.left, root.right)
-}
-
-fun isSymmetric(lhs: TreeNode?, rhs: TreeNode?): Boolean {
-    val ls: Stack<TreeNode> = Stack()
-    val rs: Stack<TreeNode> = Stack()
-    var lc = lhs
-    var rc = rhs
-    while ((lc != null || ls.isNotEmpty()) || (rc != null || rs.isNotEmpty())) {
-        if (lc?.`val` != rc?.`val`) return false
-        while (lc != null) {
-            ls.add(lc)
-            lc = lc.left
-        }
-        while (rc != null) {
-            rs.add(rc)
-            rc = rc.right
-        }
-        if (ls.map { it.`val` } != rs.map { it.`val` }) return false
-        lc = ls.pop()
-        rc = rs.pop()
-        lc = lc.right
-        rc = rc.left
-    }
-    return true
-}
-
-fun isSymmetricRecursive(lhs: TreeNode?, rhs: TreeNode?): Boolean {
-    if (lhs == null && rhs == null) return true
-    if (lhs == null || rhs == null) return false
-    if (lhs.`val` == rhs.`val`) {
-        return isSymmetricRecursive(lhs.right, rhs.left) && isSymmetricRecursive(lhs.left, rhs.right)
-    }
-    return false
-}
-
 fun levelOrder(root: TreeNode?): List<List<Int>> {
     return travel(root, false)
 }
--- src/main/kotlin/zhewuzhou/me/tree/SymmetricTree101.kt
+++ src/main/kotlin/zhewuzhou/me/tree/SymmetricTree101.kt
@@ -1,6 +1,7 @@
 package zhewuzhou.me.tree
 
 import zhewuzhou.utils.TreeNode
+import java.util.*
 
 fun isSymmetric(root: TreeNode?): Boolean {
     fun isSymmetric(lhs: TreeNode?, rhs: TreeNode?): Boolean {
@@ -15,4 +16,36 @@ fun isSymmetric(root: TreeNode?): Boolean {
     }
     if (root == null) return true
     return isSymmetric(root.left, root.right)
+}
+
+fun isSymmetricStack(root: TreeNode?): Boolean {
+    fun stackTravel(lhs: TreeNode?, rhs: TreeNode?): Boolean {
+        val ls: Stack<TreeNode> = Stack()
+        val rs: Stack<TreeNode> = Stack()
+        var lc = lhs
+        var rc = rhs
+        while ((lc != null || ls.isNotEmpty()) || (rc != null || rs.isNotEmpty())) {
+            if (lc?.`val` != rc?.`val`) return false
+            while (lc != null) {
+                ls.add(lc)
+                lc = lc.left
+            }
+            while (rc != null) {
+                rs.add(rc)
+                rc = rc.right
+            }
+            if (ls.map { it.`val` } != rs.map { it.`val` }) return false
+            lc = ls.pop()
+            rc = rs.pop()
+            lc = lc.right
+            rc = rc.left
+        }
+        return true
+    }
+
+    val lhs = root?.left
+    val rhs = root?.right
+    if (lhs == null && rhs == null) return true
+    if (lhs == null || rhs == null) return false
+    return stackTravel(root.left, root.right)
 }
