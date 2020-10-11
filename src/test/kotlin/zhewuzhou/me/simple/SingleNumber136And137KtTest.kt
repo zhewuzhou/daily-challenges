e64d787b01796529846dec14e41b083eb3483e63
zhewuzhou <zhewuzhou@gmail.com>
2020-10-11 16:29:02 +0800

    [Z.Z][Refactor] normalization of the 136/137th challenge

--- src/test/kotlin/zhewuzhou/me/simple/SingleNumber136And137KtTest.kt
+++ src/test/kotlin/zhewuzhou/me/simple/SingleNumber136And137KtTest.kt
@@ -1,12 +1,14 @@
-package zhewuzhou.me.leetcode140
+package zhewuzhou.me.simple
 
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
+import zhewuzhou.me.leetcode140.singleNumber
+import zhewuzhou.me.leetcode140.singleNumberTwo
 import java.util.*
 
-internal class LeetCode136KtTest {
+internal class SingleNumber136And137KtTest {
     private companion object {
         @JvmStatic
         fun twoCases() = Arrays.stream(
