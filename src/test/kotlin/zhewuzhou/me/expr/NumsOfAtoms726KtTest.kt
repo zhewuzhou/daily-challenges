addd1ef41429ffa63069db8afab5637907b40702
zhewuzhou <zhewuzhou@gmail.com>
2020-10-12 21:54:39 +0800

    [Z.Z][Refactor] move 726 to expr

--- src/test/kotlin/zhewuzhou/me/expr/NumsOfAtoms726KtTest.kt
+++ src/test/kotlin/zhewuzhou/me/expr/NumsOfAtoms726KtTest.kt
@@ -1,9 +1,10 @@
-package zhewuzhou.me.numberssort
+package zhewuzhou.me.expr
 
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
+import zhewuzhou.me.numberssort.countOfAtoms
 import java.util.*
 
 /*
