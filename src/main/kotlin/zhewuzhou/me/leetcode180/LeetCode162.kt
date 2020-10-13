d1e8a0bfab68c4fbe8210b2b7b802971351014ff
zhewuzhou <zhewuzhou@gmail.com>
2020-10-13 13:59:18 +0800

    [Z.Z][Refactor] normalization of the 162rd challenge

--- src/main/kotlin/zhewuzhou/me/leetcode180/LeetCode162.kt
+++ src/main/kotlin/zhewuzhou/me/leetcode180/LeetCode162.kt
@@ -2,24 +2,6 @@ package zhewuzhou.me.leetcode180
 
 fun findPeakElement(nums: IntArray): Int {
     if (nums.isEmpty()) return -1
-    for (i in nums.indices) {
-        if (i == 0) {
-            if (nums.size == 1 || nums[0] > nums[1])
-                return 0
-        } else if (i == nums.lastIndex) {
-            if (nums[i] > nums[i - 1])
-                return i
-        } else {
-            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
-                return i
-            }
-        }
-    }
-    return -1
-}
-
-fun findPeakElementFast(nums: IntArray): Int {
-    if (nums.isEmpty()) return -1
     var low = 0
     var high = nums.lastIndex
     while (low < high) {
--- src/test/kotlin/zhewuzhou/me/leetcode180/LeetCode162KtTest.kt
+++ src/test/kotlin/zhewuzhou/me/leetcode180/LeetCode162KtTest.kt
@@ -12,7 +12,7 @@ internal class LeetCode162KtTest {
         fun cases() = Arrays.stream(
             arrayOf(
                 Pair(intArrayOf(1, 2, 3, 1), 2),
-                Pair(intArrayOf(1, 2, 1, 3, 5, 6, 4), 1),
+                Pair(intArrayOf(1, 2, 1, 3, 5, 6, 4), 5),
                 Pair(intArrayOf(1, 2), 1),
                 Pair(intArrayOf(2, 1), 0),
                 Pair(intArrayOf(1), 0),
