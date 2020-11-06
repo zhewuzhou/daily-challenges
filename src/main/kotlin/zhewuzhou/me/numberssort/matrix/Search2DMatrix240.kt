b51d774f9db2d5c7f75f2f4fb6bdda86e98f2d6c
zhewuzhou <zhewuzhou@gmail.com>
2020-11-06 20:51:12 +0800

    [Z.Z][Refactoring] Magic 240 matrix search solved in really simple approach

---
+++ src/main/kotlin/zhewuzhou/me/numberssort/matrix/Search2DMatrix240.kt
@@ -0,0 +1,24 @@
+package zhewuzhou.me.numberssort.matrix
+
+class MatrixSearch {
+    companion object {
+        fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
+            if (matrix.isEmpty() || matrix[0].isEmpty()) {
+                return false
+            }
+            var col: Int = matrix[0].size - 1
+            var row = 0
+            while (col >= 0 && row <= matrix.size - 1) {
+                if (target == matrix[row][col]) {
+                    return true
+                } else if (target < matrix[row][col]) {
+                    col--
+                } else if (target > matrix[row][col]) {
+                    row++
+                }
+            }
+            return false
+        }
+    }
+}
+
