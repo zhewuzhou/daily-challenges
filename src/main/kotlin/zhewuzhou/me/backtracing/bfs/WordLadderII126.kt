d07fee7ee520c9868b3269b6feb9774a68a50bc2
zhewuzhou <zhewuzhou@gmail.com>
2020-11-24 13:18:58 +0800

    [Z.Z][Extra] BFS/DFS mixed solution

--- src/main/kotlin/zhewuzhou/me/backtracing/bfs/WordLadderII126.kt
+++ src/main/kotlin/zhewuzhou/me/backtracing/bfs/WordLadderII126.kt
@@ -3,11 +3,11 @@ package zhewuzhou.me.backtracing.bfs
 import java.util.*
 
 
-fun findLadders(beginWord: String, endWord: String, wordList: List<String>): List<List<String>> {
+fun findLaddersConcise(beginWord: String, endWord: String, wordList: List<String>): List<List<String>> {
     val wordSet: MutableSet<String> = HashSet(wordList)
     if (!wordSet.contains(endWord)) return ArrayList()
     var layer = mutableMapOf(
-        beginWord to mutableListOf(mutableListOf(beginWord))
+            beginWord to mutableListOf(mutableListOf(beginWord))
     )
     while (layer.isNotEmpty()) {
         val newLayer = mutableMapOf<String, MutableList<MutableList<String>>>()
@@ -36,4 +36,73 @@ fun findLadders(beginWord: String, endWord: String, wordList: List<String>): Lis
         }
     }
     return ArrayList()
+}
+
+private fun getNeighbors(node: String, dict: Set<String>): List<String> {
+    val res = mutableListOf<String>()
+    val chs = node.toCharArray()
+    var c = 'a'
+    while (c <= 'z') {
+        for (i in chs.indices) {
+            if (chs[i] == c) continue
+            val oldC = chs[i]
+            chs[i] = c
+            if (dict.contains(String(chs))) {
+                res.add(String(chs))
+            }
+            chs[i] = oldC
+        }
+        c++
+    }
+    return res
+}
+
+
+fun findLadders(start: String, end: String, wordList: List<String>): List<List<String>> {
+    val dict = HashSet(wordList)
+    val res = mutableListOf<List<String>>()
+    val nbs = mutableMapOf<String, MutableList<String>>()
+    val distance = mutableMapOf<String, Int>()
+    dict.add(start)
+    fun bfs() {
+        for (str in dict) nbs[str] = ArrayList()
+        val queue: Queue<String> = LinkedList()
+        queue.offer(start)
+        distance[start] = 0
+        while (!queue.isEmpty()) {
+            val count: Int = queue.size
+            var foundEnd = false
+            for (i in 0 until count) {
+                val cur = queue.poll()
+                val curDistance = distance[cur]!!
+                val neighbors: List<String> = getNeighbors(cur, dict)
+                for (neighbor in neighbors) {
+                    nbs[cur]!!.add(neighbor)
+                    if (!distance.containsKey(neighbor)) { // Check if visited
+                        distance[neighbor] = curDistance + 1
+                        if (end == neighbor) // Found the shortest path
+                            foundEnd = true else queue.offer(neighbor)
+                    }
+                }
+            }
+            if (foundEnd) break
+        }
+    }
+
+    fun dfs(cur: String, comb: MutableList<String>, res: MutableList<List<String>>) {
+        comb.add(cur)
+        if (end == cur) {
+            res.add(listOf(*comb.toTypedArray()))
+        } else {
+            for (next in nbs[cur]!!) {
+                if (distance[next] == distance[cur]!! + 1) {
+                    dfs(next, comb, res)
+                }
+            }
+        }
+        comb.removeAt(comb.size - 1)
+    }
+    bfs()
+    dfs(start, mutableListOf(), res)
+    return res
 }
