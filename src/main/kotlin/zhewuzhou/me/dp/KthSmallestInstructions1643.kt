53fd0725beb2f1e939b522f0969cc9f8afcce42f
Zhewu Zhou <zhewuzhou@Zhewus-Mac-mini.local>
2020-11-27 09:32:26 +0800

    [Z.Z][Hard] V & H path in k index

--- src/main/kotlin/zhewuzhou/me/dp/KthSmallestInstructions1643.kt
+++ src/main/kotlin/zhewuzhou/me/dp/KthSmallestInstructions1643.kt
@@ -1,10 +1,41 @@
 package zhewuzhou.me.dp
 
+import java.lang.StringBuilder
+
 /**
  * destination.length == 2
  * 1 <= row, column <= 15
- * 1 <= k <= nCr(row + column, row), where nCr(a, b) denotes a choose b​​​​​.
+ * 1 <= k <= nCr(row + column, row), where nCr(a, b) denotes a choose b
  */
+
+fun calcComb(n: Int): Array<IntArray> {
+    val metrics = Array(n + 1) {
+        IntArray(n + 1) { 1 }
+    }
+    for (r in 2..n) {
+        for (c in 1 until r) {
+            metrics[r][c] = metrics[r - 1][c - 1] + metrics[r - 1][c]
+        }
+    }
+    return metrics
+}
+
 fun kthSmallestPath(destination: IntArray, k: Int): String {
-    return ""
+    val comb = calcComb(destination[0] + destination[1])
+    val sb = StringBuilder()
+    var rem = k
+    var numH = destination[1]
+    var numV = destination[0]
+    while (k > 0) {
+        val maxIndexOfH = comb[numH - 1 + numV][numV]
+        if (k <= maxIndexOfH) {
+            sb.append('H')
+            numH -= 1
+        } else {
+            sb.append('V')
+            numV -= 1
+            rem -= maxIndexOfH
+        }
+    }
+    return sb.toString()
 }
--- src/test/kotlin/zhewuzhou/me/dp/KthSmallestInstructions1643KtTest.kt
+++ src/test/kotlin/zhewuzhou/me/dp/KthSmallestInstructions1643KtTest.kt
@@ -2,6 +2,7 @@ package zhewuzhou.me.dp
 
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.*
@@ -23,5 +24,16 @@ internal class KthSmallestInstructions1643KtTest {
     @MethodSource("cases")
     fun `Should find kth smallest instructions`(case: Triple<IntArray, Int, String>) {
         assertThat(kthSmallestPath(case.first, case.second), `is`(case.third))
+    }
+
+    @Test
+    fun `Should find the comb of n`() {
+        val comb = calcComb(6)
+
+        assertThat(comb[5][1], `is`(5))
+        assertThat(comb[5][2], `is`(10))
+        assertThat(comb[5][3], `is`(10))
+        assertThat(comb[5][4], `is`(5))
+        assertThat(comb[6][2], `is`(15))
     }
 }
