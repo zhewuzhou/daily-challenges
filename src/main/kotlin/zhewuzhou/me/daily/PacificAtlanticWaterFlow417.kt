6c7863305b928be36db40a3521f3d11f1425a566
zhewuzhou <zhewuzhou@gmail.com>
2021-01-04 22:53:32 +0800

    [Z.Z][DFS] About 40 mins of dfs challenges

---
+++ src/main/kotlin/zhewuzhou/me/daily/PacificAtlanticWaterFlow417.kt
@@ -0,0 +1,80 @@
+fun pacificAtlantic(matrix: Array<IntArray>): List<List<Int>> {
+    if (matrix.isEmpty() || matrix[0].isEmpty()) {
+        return listOf()
+    }
+
+    val result = mutableSetOf<List<Int>>()
+    val pacificCache = mutableSetOf(
+        Pair(0, matrix[0].lastIndex),
+        Pair(matrix.lastIndex, 0)
+    )
+
+    val atlanticCache = mutableSetOf(
+        Pair(0, matrix[0].lastIndex),
+        Pair(matrix.lastIndex, 0)
+    )
+
+    fun nextSteps(cur: Pair<Int, Int>, visited: List<Pair<Int, Int>>): List<Pair<Int, Int>> {
+        return listOf(
+            Pair(cur.first + 1, cur.second),
+            Pair(cur.first - 1, cur.second),
+            Pair(cur.first, cur.second + 1),
+            Pair(cur.first, cur.second - 1)
+        ).filter {
+            it.first in (0..matrix.lastIndex) &&
+                    it.second in (0..matrix[0].lastIndex) &&
+                    matrix[cur.first][cur.second] >= matrix[it.first][it.second] &&
+                    !visited.contains(it)
+        }
+    }
+
+    fun canMoveToPacific(grid: Pair<Int, Int>, visited: MutableList<Pair<Int, Int>>): Boolean {
+        if (pacificCache.contains(grid) ||
+            grid.first == 0 ||
+            grid.second == 0
+        ) {
+            visited.add(grid)
+            pacificCache.addAll(visited)
+            return true
+        } else {
+            for (g in nextSteps(grid, visited)) {
+                visited.add(g)
+                if (canMoveToPacific(g, visited)) {
+                    return true
+                }
+                visited.removeAt(visited.lastIndex)
+            }
+        }
+        return false
+    }
+
+    fun canMoveToAtlantic(grid: Pair<Int, Int>, visited: MutableList<Pair<Int, Int>>): Boolean {
+        if (pacificCache.contains(grid) ||
+            grid.first == matrix.lastIndex ||
+            grid.second == matrix[0].lastIndex
+        ) {
+            visited.add(grid)
+            atlanticCache.addAll(visited)
+            return true
+        } else {
+            for (g in nextSteps(grid, visited)) {
+                visited.add(g)
+                if (canMoveToAtlantic(g, visited)) {
+                    return true
+                }
+                visited.removeAt(visited.lastIndex)
+            }
+        }
+        return false
+    }
+
+    for (r in 0..matrix.lastIndex) {
+        for (c in 0..matrix[0].lastIndex) {
+            val grid = Pair(r, c)
+            if (canMoveToAtlantic(grid, mutableListOf()) && canMoveToPacific(grid, mutableListOf())) {
+                result.add(listOf(r, c))
+            }
+        }
+    }
+    return result.toList()
+}
---
+++ src/test/kotlin/zhewuzhou/me/daily/PacificAtlanticWaterFlow417KtTest.kt
@@ -0,0 +1,56 @@
+package zhewuzhou.me.daily
+
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import pacificAtlantic
+import java.util.*
+
+internal class PacificAtlanticWaterFlow417KtTest {
+    private companion object {
+        @JvmStatic
+        fun cases() = Arrays.stream(
+            arrayOf(
+                Pair(
+                    arrayOf(
+                        intArrayOf(
+                            1, 2, 2, 3, 5
+                        ),
+                        intArrayOf(
+                            3, 2, 3, 4, 4
+                        ),
+                        intArrayOf(
+                            2, 4, 5, 3, 1
+                        ),
+                        intArrayOf(
+                            6, 7, 1, 4, 5
+                        ),
+                        intArrayOf(
+                            5, 1, 1, 2, 4
+                        )
+                    ),
+                    listOf(
+                        listOf(0, 4),
+                        listOf(1, 3),
+                        listOf(1, 4),
+                        listOf(2, 2),
+                        listOf(3, 0),
+                        listOf(3, 1),
+                        listOf(4, 0)
+                    )
+                )
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("cases")
+    fun `Should find all of the grids that can flow to both pacific and atlantic`(case: Pair<Array<IntArray>, List<List<Int>>>) {
+        val result = pacificAtlantic(case.first)
+        assertThat(result.size, `is`(case.second.size))
+        for (p in case.second) {
+            assertThat(result.contains(p), `is`(true))
+        }
+    }
+}
